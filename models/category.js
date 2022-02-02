cee13924fb16a840471c1a78ddf9c60991d1cec4
Ken Yamada <yamakenth@gmail.com>
2022-02-02 21:30:16 +0700

    Create CategorySchema

---
+++ models/category.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var CategorySchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true }
+});
+
+CategorySchema
+  .virtual('url')
+  .get(function() {
+    return '/catalog/category' + this._id;
+  });
+
+module.exports = mongoose.model('Category', CategorySchema);
