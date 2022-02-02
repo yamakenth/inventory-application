ea935fadc2bef793f0bf97c9735711656857b280
Ken Yamada <yamakenth@gmail.com>
2022-02-02 21:12:55 +0700

    Connect to MongoDB

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// set up mongoose connection 
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://inventory-application:inventory-application-123@cluster0.jqgnq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
