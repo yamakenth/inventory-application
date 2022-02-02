dc83c0689f636641126e235204eba688790271eb
Ken Yamada <yamakenth@gmail.com>
2022-02-02 21:31:42 +0700

    Create ManufacturerSchema

---
+++ models/manufacturer.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ManufacturerSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 }
+});
+
+ManufacturerSchema
+  .virtual('url')
+  .get(function() {
+    return '/catalog/manufacturer' + this._id;
+  });
+
+module.exports = mongoose.model('Manufacturer', ManufacturerSchema);
