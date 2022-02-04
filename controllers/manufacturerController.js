f75a932fbcb7fac626eabe4e9a24203f698e4e79
Ken Yamada <yamakenth@gmail.com>
2022-02-04 16:40:57 +0700

    Create manufacturer list view

--- controllers/manufacturerController.js
+++ controllers/manufacturerController.js
@@ -1,8 +1,13 @@
 var Manufacturer = require('../models/manufacturer');
 
 // display list of all manufacturers 
-exports.manufacturer_list = function(req, res) {
-  res.send('NOT IMPLEMENTED: manufacturer_list');
+exports.manufacturer_list = function(req, res, next) {
+  Manufacturer.find()
+  .sort({ name: 1 })
+  .exec(function(err, list_manufacturer) {
+    if (err) return next(err);
+    res.render('manufacturer_list', { title: 'Manufacturer List', manufacturer_list: list_manufacturer });
+  });
 }
 
 // display detail page for a specific manufacturer 
